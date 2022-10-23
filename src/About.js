75c45bad1b7f91696c9ef93ae6e93289adc4b320
EZGI COBAN <SARAHEZGICOBAN@GMAIL.COM>
2022-10-22 18:52:04 -0700

    completed book component task

--- src/About.js
+++ src/About.js
@@ -4,7 +4,15 @@ class Profile extends Component {
 
   render() {
     /* TODO: render information about the developers */
-    return <p>Profile page coming soon</p>
+    return (
+      <>
+      <h1>Project Developers</h1>
+      <h3>Chris Lopez</h3>
+      <p>Christopher Lopez is a 33 year software developer from from Los Angeles who is seeking to make music production a collaborative effort again. </p>
+      <h3>Ezgi Coban</h3>
+      <p>Software Developer based in Seattle, studying JavaScript at Code Fellows.</p>
+      </>
+      )
   }
 };
 
